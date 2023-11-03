a17a6f346ab9791f133e2b645fa46465a9bb9334
andie <zchangan@163.com>
2023-11-03 12:01:44 +0800

    switch to pydantic

--- conf.py
+++ conf.py
@@ -1,17 +1,18 @@
 import json
 import os
 import dotenv
-from typing import TypedDict
+from pydantic import BaseModel, Field
 
 
-class Resource(TypedDict):
+class Resource(BaseModel):
     azure_subscription_id: str
     azure_resource_group: str
     azure_resource_name: str
     start_time: str
+    scale: float = Field(default=1)
 
 
-class Task(TypedDict):
+class Task(BaseModel):
     name: str
     bitable_app_token: str
     bitable_table_id: str
@@ -25,4 +26,4 @@ azure_app_client_id = os.environ.get('AZURE_APP_CLIENT_ID')
 azure_app_client_secret = os.environ.get('AZURE_APP_CLIENT_SECRET')
 feishu_app_id = os.environ.get('FEISHU_APP_ID')
 feishu_app_secret = os.environ.get('FEISHU_APP_SECRET')
-tasks: list[Task] = json.loads(os.environ.get('TASKS'))
+tasks: list[Task] = [Task.model_validate(one) for one in json.loads(os.environ.get('TASKS'))]
--- main.py
+++ main.py
@@ -8,42 +8,42 @@ import pytz
 tz8 = pytz.timezone('Asia/Shanghai')
 
 for task in conf.tasks:
-    print(f"processing {task['name']}")
+    print(f'processing {task.name}')
     feishu = Feishu(conf.feishu_app_id, conf.feishu_app_secret)
-    bitable = feishu.bitable(task['bitable_app_token'])
-    table = bitable.table(task['bitable_table_id'])
-    for resource in task['resources']:
+    bitable = feishu.bitable(task.bitable_app_token)
+    table = bitable.table(task.bitable_table_id)
+    for resource in task.resources:
         last_records = table.list(limit=1, sort=['时间 DESC'],
-                                  filter=f"CurrentValue.[资源名]=\"{resource['azure_resource_name']}\"")
+                                  filter=f"CurrentValue.[资源名]=\"{resource.azure_resource_name}\"")
         if len(last_records) > 0:
             start_time = datetime.fromtimestamp(last_records[0].fields['时间'] / 1000).astimezone(tz8)
             start_time -= timedelta(hours=1)
             print(f"got start time {start_time} from records")
         else:
-            start_time = datetime.fromisoformat(resource['start_time']).astimezone(tz8)
+            start_time = datetime.fromisoformat(resource.start_time).astimezone(tz8)
             print(f"got start time {start_time} from task")
         end_time = datetime.now().astimezone(tz8)
         if end_time <= start_time:
-            print(f"skip {resource['azure_resource_name']} because of too short time range")
+            print(f"skip {resource.azure_resource_name} because of too short time range")
             continue
 
         m = OpenAIMetrics(
             tenant_id=conf.azure_app_tenant_id,
             client_id=conf.azure_app_client_id,
             client_secret=conf.azure_app_client_secret,
-            subscription_id=resource['azure_subscription_id'],
-            resource_group=resource['azure_resource_group'],
-            resource_name=resource['azure_resource_name']
+            subscription_id=resource.azure_subscription_id,
+            resource_group=resource.azure_resource_group,
+            resource_name=resource.azure_resource_name
         )
 
-        print(f"getting metrics for {resource['azure_resource_name']} from {start_time} to {end_time}")
+        print(f"getting metrics for {resource.azure_resource_name} from {start_time} to {end_time}")
         data = m.get(['ProcessedPromptTokens', 'GeneratedTokens'], start_time, end_time)
         records = []
         for one_metric in data.value:
             for one_model in one_metric.timeseries:
                 for one_time in one_model.data:
                     records.append({
-                        '资源名': resource['azure_resource_name'],
+                        '资源名': resource.azure_resource_name,
                         '时间': one_time.time_stamp.timestamp() * 1000,
                         '模型': one_model.metadatavalues[0].value,
                         'Prompt Tokens': one_time.total if one_metric.name.value == 'ProcessedPromptTokens' else 0,
@@ -52,6 +52,8 @@ for task in conf.tasks:
 
         df = pd.DataFrame(records).groupby(['资源名', '时间', '模型']).sum().reset_index()
         df = df[(df['Prompt Tokens'] > 0) | (df['Completion Tokens'] > 0)]
+        df['Prompt Tokens'] = (df['Prompt Tokens'] * resource.scale)
+        df['Completion Tokens'] = (df['Completion Tokens'] * resource.scale)
         final_records = df.to_dict('records')
         if len(final_records) > 0:
             print(f"upserting {len(final_records)} records")
