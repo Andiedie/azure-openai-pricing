946f6dbca701abe3dd6c0e40cfa3ae721ea6030e
andie <zchangan@163.com>
2023-11-10 23:33:19 +0800

    accept base64 tasks

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
+import base64
+import binascii
 import json
 import os
 import dotenv
@@ -26,4 +28,10 @@ azure_app_client_id = os.environ.get('AZURE_APP_CLIENT_ID')
 azure_app_client_secret = os.environ.get('AZURE_APP_CLIENT_SECRET')
 feishu_app_id = os.environ.get('FEISHU_APP_ID')
 feishu_app_secret = os.environ.get('FEISHU_APP_SECRET')
-tasks: list[Task] = [Task.model_validate(one) for one in json.loads(os.environ.get('TASKS'))]
+try:
+    _raw_task_data = base64.b64decode(os.environ.get('TASKS', ''), validate=True).decode()
+except binascii.Error:
+    _raw_task_data = os.environ.get('TASKS', '')
+for one in json.loads(_raw_task_data):
+    print(one)
+tasks: list[Task] = [Task.model_validate(one) for one in json.loads(_raw_task_data)]
